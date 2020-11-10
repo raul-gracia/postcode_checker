176bab6762a2021f093590823ee5d511598ca05f
Raul Gracia <raulgracialario@gmail.com>
2020-11-10 16:43:50 +0000

    Rubocop, Rspec and SimpleCov

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root to: redirect("/dashboard")
-  get "/dashboard", to: "dashboard#index"
-  post "/dashboard", to: "dashboard#check"
+  root to: redirect('/dashboard')
+  get '/dashboard', to: 'dashboard#index'
+  post '/dashboard', to: 'dashboard#check'
 end
